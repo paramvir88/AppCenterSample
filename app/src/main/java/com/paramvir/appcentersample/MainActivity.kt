707d9776c5b7cc0fb68cce88d417dbdb8fc82e73
Paramvir <paramvir@fastrallc.com>
2018-12-04 11:27:33 +0530

    Initial commit

---
+++ app/src/main/java/com/paramvir/appcentersample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.paramvir.appcentersample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
