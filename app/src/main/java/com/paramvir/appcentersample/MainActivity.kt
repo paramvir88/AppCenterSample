93ecf53e9099b10e71e3da3b4f88244c26c65834
Paramvir <paramvir@fastrallc.com>
2018-12-04 11:32:55 +0530

    integrated appcenter

--- app/src/main/java/com/paramvir/appcentersample/MainActivity.kt
+++ app/src/main/java/com/paramvir/appcentersample/MainActivity.kt
@@ -2,11 +2,15 @@ package com.paramvir.appcentersample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(application, "38d6b371-11ca-498d-9909-c45b50c87a9d", Analytics::class.java, Crashes::class.java)
     }
 }
